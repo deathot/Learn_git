1d9e2df7e9792b0d962a99dc442cccca15174497
deathot <deathot717@gmail.com>
2024-08-06 11:12:59 +0800

    add 3 files

---
+++ hi.py
@@ -0,0 +1,9 @@
+'''
+Git is version control system.
+Git is free software
+hey
+Git is a distributed version control system. 
+whu no diff
+i got konw
+Git has a mutable index called stage
+'''
---
+++ hi1.py
@@ -0,0 +1,7 @@
+'''
+this is a sub pro
+git tracks changes
+修改前以add但未提交
+new dev branch
+why
+'''
